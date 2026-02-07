09618ffeae55ff58f80ce878c5cd7d8df9af177e
Tauseef Ahmad <ertauseefahmad@gmail.com>
2026-02-07 20:40:52 +0530

    Added github window

--- src/components/windows/MacWindow.jsx
+++ src/components/windows/MacWindow.jsx
@@ -2,18 +2,25 @@ import React from 'react'
 import { Rnd } from 'react-rnd'
 import "./window.scss"
 
-const MacWindow = ({ children }) => {
+const MacWindow = ({ children, width = "40vw", height = "40vh", windowName, setWindowsState }) => {
     return (
-        <Rnd>
+        <Rnd
+            default={{
+                width: width,
+                height: height,
+                x: 300,
+                y: 200
+            }}
+        >
             <div className="window">
                 <div className="nav">
                     <div className="dots">
-                        <div className="dot red"></div>
+                        <div onClick={() => setWindowsState(state => ({ ...state, [windowName]: false }))} className="dot red"></div>
                         <div className="dot yellow"></div>
                         <div className="dot green"></div>
                     </div>
 
-                    <div className="title"><p>tauseefahmad - zsh</p></div>
+                    <div className="title"><p>ankurprajapti - zsh</p></div>
 
                 </div>
                 <div className="main-content">
